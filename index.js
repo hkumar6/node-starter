999b8bc00df38a2001ab9a1fb035bfe626caded0
Kumar Harsha <kumar.harsha@tum.de>
2017-03-09 11:56:00 +0100

    fix token

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/api/todos/:id', todos.one)
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+      req.query['hub.verify_token'] === 'batman_bot') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
